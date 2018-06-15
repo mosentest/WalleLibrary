8eee7e521838bc04c5b6372351eda58eb2b9b304
ziqi.mo <ziqi.mo@sao.so>
2018-06-15 16:00:47 +0800

    frist commit

---
+++ app/src/main/java/mo/wall/org/MainActivity.kt
@@ -0,0 +1,11 @@
+package mo.wall.org
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
