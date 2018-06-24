f88073ee49085600c28d63cc991721bfb143a711
moziqi <moziqi@adflash.cn>
2018-06-24 01:50:43 -0800

    提交自适应的代码

---
+++ library_ui/src/main/java/org/wall/mo/ui/autolayout/AutoDensity.java
@@ -0,0 +1,101 @@
+package org.wall.mo.ui.autolayout;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+import android.util.DisplayMetrics;
+
+/**
+ * @author ziqi-mo
+ * @dec 来自：https://mp.weixin.qq.com/s/d9QCoBP6kV9VSWvVldVVwA
+ * 感谢今日头条提供的方案
+ */
+public class AutoDensity {
+
+
+    private static AutoDensity instance;
+
+    private double widthPX, heightPX;
+
+    private float sNoncompatDensity;
+    private float sNoncompatScaledDensity;
+    private double targetDpi;
+
+    private AutoDensity() {
+
+    }
+
+
+    public static AutoDensity getInstance() {
+        if (instance == null) {
+            synchronized (AutoDensity.class) {
+                if (instance == null) {
+                    instance = new AutoDensity();
+                }
+            }
+        }
+        return instance;
+    }
+
+    /**
+     * 在activity的onCreate初始化
+     *
+     * @param activity
+     * @param application
+     */
+    public void setCustomDensity(Activity activity, final Application application) {
+        final DisplayMetrics appDisplayMetrics = application.getResources().getDisplayMetrics();
+        if (Float.compare(sNoncompatDensity, 0f) == 0) {
+            //原本的density
+            sNoncompatDensity = appDisplayMetrics.density;
+            ////原本的scaledDensity
+            sNoncompatScaledDensity = appDisplayMetrics.scaledDensity;
+            application.registerComponentCallbacks(new ComponentCallbacks() {
+                @Override
+                public void onConfigurationChanged(Configuration newConfig) {
+                    if (newConfig != null && newConfig.fontScale > 0) {
+                        sNoncompatScaledDensity = application.getResources().getDisplayMetrics().scaledDensity;
+                    }
+                }
+
+                @Override
+                public void onLowMemory() {
+
+                }
+            });
+        }
+        //android中的dp在渲染前会将dp转为px，计算公式：
+        //px = density * dp;
+        //density = dpi / 160;
+        //px = dp * (dpi / 160);
+        //获取宽的dp
+        float wdp = (float) (widthPX / (targetDpi / 160));
+        float targetDensity = appDisplayMetrics.widthPixels / wdp;
+        final int targetDensityDpi = (int) (160 * targetDensity);
+        float targetScaledDensity = targetDensity * (sNoncompatScaledDensity / sNoncompatDensity);
+
+        appDisplayMetrics.density = targetDensity;
+        appDisplayMetrics.densityDpi = targetDensityDpi;
+        appDisplayMetrics.scaledDensity = targetScaledDensity;
+
+        final DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
+
+        activityDisplayMetrics.density = targetDensity;
+        activityDisplayMetrics.densityDpi = targetDensityDpi;
+        activityDisplayMetrics.scaledDensity = targetScaledDensity;
+    }
+
+    /**
+     * 设置相应的UI 宽高图
+     *
+     * @param widthPX
+     * @param heightPX
+     * @param size     手机尺寸 一般都是以苹果设计，4.7寸
+     */
+    public void setUiDesign(double widthPX, double heightPX, double size) {
+        this.widthPX = widthPX;
+        this.heightPX = heightPX;
+        targetDpi = Math.sqrt(this.widthPX * this.widthPX + this.heightPX * this.heightPX) / size;
+    }
+}
---
+++ library_ui/src/main/java/org/wall/mo/ui/autolayout/AutoLayoutHelper.java
@@ -0,0 +1,120 @@
+package org.wall.mo.ui.autolayout;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by ziqi-mo on 2017/4/2 0002.
+ * 实现代码动态适配，用起来麻烦，但是可以减少dimen.xml文件
+ */
+
+public class AutoLayoutHelper {
+    public final static String TAG = "AutoLayoutHelper";
+
+    private static AutoLayoutHelper instance;
+
+    private Context context;
+
+    private int currentWidth;
+
+    private int currentHeight;
+
+    private int defaultWidth;
+
+    private int defaultHeight;
+
+    private int mOrientation;
+
+    private AutoLayoutHelper(Context context) {
+        this.context = context;
+        int[] screenSize = ScreenUtils.getScreenSize(this.context, false);
+        currentWidth = screenSize[0];
+        currentHeight = screenSize[1];
+    }
+
+    public static AutoLayoutHelper getInstance(Context context) {
+        if (context == null) {
+            throw new RuntimeException("AutoLayoutHelper context null");
+        }
+        //不适合，如果考虑横竖，这种方案不好
+        if (instance == null) {
+            synchronized (AutoLayoutHelper.class) {
+                if (instance == null) {
+                    Context temp = context.getApplicationContext();
+                    instance = new AutoLayoutHelper(temp);
+                }
+            }
+        }
+        return instance;
+        //直接创建对象吧
+        //return new AutoLayoutHelper(context.getApplicationContext());
+    }
+
+    /**
+     * 美术的设计图 比如 720*1280
+     *
+     * @param widthPX
+     * @param heightPX
+     */
+    public void initDefaultUISize(int widthPX, int heightPX) {
+        defaultWidth = widthPX;
+        defaultHeight = heightPX;
+    }
+
+    public int getTargetPX(int UIpx) {
+        return UIpx * Math.min(currentWidth, currentHeight) / Math.min(defaultWidth, defaultHeight);
+    }
+
+    /**
+     * 设计图标记的宽
+     *
+     * @param widthUIpx
+     * @return
+     */
+    private int getTargetWidthPX(int widthUIpx) {
+        return widthUIpx * currentWidth / defaultWidth;
+    }
+
+    /**
+     * 设计图标记的高
+     *
+     * @param heightUIpx
+     * @return
+     */
+    private int getTargetHeightPX(int heightUIpx) {
+        return heightUIpx * currentHeight / defaultHeight;
+    }
+
+    public void updateOrientation(int orientation) {
+        this.mOrientation = orientation;
+    }
+
+    public LinearLayout.LayoutParams getLinearLayoutLayoutParams(int widthUIpx, int heightUIpx) {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(getTargetWidthPX(widthUIpx), getTargetHeightPX(heightUIpx));
+        return layoutParams;
+    }
+
+    public RelativeLayout.LayoutParams getRelativeLayoutLayoutParams(int widthUIpx, int heightUIpx) {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(getTargetWidthPX(widthUIpx), getTargetHeightPX(heightUIpx));
+        return layoutParams;
+    }
+
+    public FrameLayout.LayoutParams getFrameLayoutLayoutParams(int widthUIpx, int heightUIpx) {
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(getTargetWidthPX(widthUIpx), getTargetHeightPX(heightUIpx));
+        return layoutParams;
+    }
+
+    public AbsListView.LayoutParams getAbsListViewLayoutParams(int widthUIpx, int heightUIpx) {
+        AbsListView.LayoutParams layoutParams = new AbsListView.LayoutParams(getTargetWidthPX(widthUIpx), getTargetHeightPX(heightUIpx));
+        return layoutParams;
+    }
+
+    public ViewGroup.LayoutParams getViewGroupLayoutParams(int widthUIpx, int heightUIpx) {
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(getTargetWidthPX(widthUIpx), getTargetHeightPX(heightUIpx));
+        return layoutParams;
+    }
+}
