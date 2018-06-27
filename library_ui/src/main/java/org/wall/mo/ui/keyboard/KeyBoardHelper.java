3f73f5bdce9dcc8edf7e34f17a9dce929eabf612
ziqi.mo <ziqi.mo@sao.so>
2018-06-27 11:48:03 +0800

    增加键盘工具类

---
+++ library_ui/src/main/java/org/wall/mo/ui/keyboard/KeyBoardHelper.java
@@ -0,0 +1,76 @@
+package org.wall.mo.ui.keyboard;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+/**
+ * 作者 create by moziqi on 2018/6/27
+ * 邮箱 709847739@qq.com
+ * 说明
+ **/
+public class KeyBoardHelper {
+
+    private boolean isVisibleForLast;
+
+    /**
+     * 用于键盘软键盘是否弹起
+     *
+     * @param activity
+     * @param listener
+     */
+    public void addOnSoftKeyBoardVisibleListener(Activity activity, final IKeyBoardVisibleListener listener) {
+        final View decorView = activity.getWindow().getDecorView();
+        decorView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect rect = new Rect();
+                decorView.getWindowVisibleDisplayFrame(rect);
+                //计算出可见屏幕的高度
+                int displayHight = rect.bottom;
+                //获得屏幕整体的高度
+                int hight = decorView.getHeight();
+                //获得键盘高度
+                int keyboardHeight = hight - displayHight;
+                boolean visible = (double) displayHight / hight < 0.8;
+                if (visible != isVisibleForLast) {
+                    listener.onSoftKeyBoardVisible(visible, keyboardHeight);
+                }
+                isVisibleForLast = visible;
+            }
+        });
+    }
+
+    public interface IKeyBoardVisibleListener {
+        void onSoftKeyBoardVisible(boolean visible, int keyboardHeight);
+    }
+
+
+    /**
+     * https://www.jb51.net/article/137229.htm
+     *
+     * @param main
+     * @param scrollView1
+     */
+    public void addLayoutScrollListener(final View main, final View scrollView1) {
+        main.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect rect = new Rect();  //1、获取main在窗体的可视区域
+                main.getWindowVisibleDisplayFrame(rect);  //2、获取main在窗体的不可视区域高度，在键盘没有弹起时，main.getRootView().getHeight()调节度应该和rect.bottom高度一样
+                int mainInvisibleHeight = main.getRootView().getHeight() - rect.bottom;
+                int screenHeight = main.getRootView().getHeight();//屏幕高度
+                //3、不可见区域大于屏幕本身高度的1/4：说明键盘弹起了
+                if (mainInvisibleHeight > screenHeight / 4) {
+                    int[] location = new int[2];
+                    scrollView1.getLocationInWindow(location);   // 4､获取Scroll的窗体坐标，算出main需要滚动的高度
+                    int srollHeight = (location[1] + scrollView1.getHeight()) - rect.bottom;   //5､让界面整体上移键盘的高度
+                    main.scrollTo(0, srollHeight);
+                } else {  //3、不可见区域小于屏幕高度1/4时,说明键盘隐藏了，把界面下移，移回到原有高度
+                    main.scrollTo(0, 0);
+                }
+            }
+        });
+    }
+}
---
+++ library_ui/src/main/java/org/wall/mo/ui/keyboard/KeyboardUtils.java
@@ -0,0 +1,53 @@
+package org.wall.mo.ui.keyboard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+public class KeyboardUtils {
+
+    /**
+     * 打开软键盘
+     *
+     * @param mEditText
+     * @param mContext
+     */
+    public static void openKeyboard(EditText mEditText, Context mContext) {
+        InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(mEditText, InputMethodManager.RESULT_SHOWN);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    /**
+     * 关闭软键盘
+     *
+     * @param mEditText 输入框
+     * @param mContext  上下文
+     */
+    public static void closeKeyboard(EditText mEditText, Context mContext) {
+        InputMethodManager imm = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+    }
+
+    /**
+     * 判断当前软键盘是否打开
+     *
+     * @param activity
+     * @return
+     */
+    public static boolean isSoftInputShow(Activity activity) {
+
+        // 虚拟键盘隐藏 判断view是否为空
+        View view = activity.getWindow().peekDecorView();
+        if (view != null) {
+            // 隐藏虚拟键盘
+            InputMethodManager inputmanger = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            // inputmanger.hideSoftInputFromWindow(view.getWindowToken(),0);
+            return inputmanger.isActive() && activity.getWindow().getCurrentFocus() != null;
+        }
+        return false;
+    }
+
+}
